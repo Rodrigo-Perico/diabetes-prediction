3b82d0fd64c4a47b891057cd73725e8ffe2080dc
Rodrigo Perico <rodrigoperico@hotmail.com>
2024-11-09 15:44:52 -0300

    Home page parcial, BD 100%

--- home/ml_model.py
+++ home/ml_model.py
@@ -1,6 +1,29 @@
 import pickle
+import os
+from django.conf import settings
 
-def load_model():
-    with open('.//modelos/KNN.pkl', 'rb') as f:
-        model = pickle.load(f)
-    return model
+# Caminho para os modelos salvos
+#KNN_MODEL_PATH = os.path.join(settings.BASE_DIR, 'home', 'modelos', 'modelo_knn.pkl')
+LOGISTIC_MODEL_PATH = os.path.join(settings.BASE_DIR, 'home', 'modelos', 'modelo_logistic.pkl')
+
+# Função para carregar o modelo KNN
+#def load_knn_model():
+#    with open(KNN_MODEL_PATH, 'rb') as f:
+#        return pickle.load(f)
+
+# Função para carregar o modelo de Regressão Logística
+def load_logistic_model():
+    with open(LOGISTIC_MODEL_PATH, 'rb') as f:
+        return pickle.load(f)
+
+# Função de predição para diabetes com o modelo KNN
+#def predict_diabetes_knn(features):
+#    model = load_knn_model()
+#    prediction = model.predict([features])
+#    return prediction[0]  # Retorna o resultado da predição
+
+# Função de predição para diabetes com o modelo de Regressão Logística
+def predict_diabetes_logistic(features):
+    model = load_logistic_model()
+    prediction = model.predict([features])
+    return prediction[0]  # Retorna o resultado da predição
--- ml_model.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-def load_model():
-    with open('.//modelos/KNN.pkl', 'rb') as f:
-        model = pickle.load(f)
-    return model
